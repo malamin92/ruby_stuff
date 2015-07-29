ad439bc2816211f70d629a7472f62702e2c540ad
Mo Amin <m.alamin92@gmail.com>
2015-07-29 14:25:44 -0400

    redid some examples for practice and refresh, added some comments

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -7,13 +7,10 @@ class Timer
 
 	def time_string
 
-		@hours = @seconds/(60*60)
-		@minutes = (@seconds/60)%60
-		@seconds = @seconds%60
+		@hours = @seconds/(60*60) #hours is = @seconds / (60*60)
+		@minutes = (@seconds/60) % 60 # minutes = seconds/60 (to get actual minutes) % 60 (get the remainder of dividing the minutes)
+		@seconds = @seconds % 60 #seconds is the remainder of seconds / 60
 
 		return "#{"%02d" % @hours}:#{"%02d" % @minutes}:#{"%02d" % @seconds}"
-
-
 	end
-
 end
