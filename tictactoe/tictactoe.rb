13545f9082c4b56af5fb3a703226a7f91ac0dfd5
Mo Amin <m.alamin92@gmail.com>
2015-08-31 01:20:25 -0400

    added error checking and draw condition to tictactoe

--- tictactoe/tictactoe.rb
+++ tictactoe/tictactoe.rb
@@ -16,9 +16,9 @@ class TicTacToe
 		end
 	end
 
-	#takes a row and column and assigns a shape to that spot depending on whose turn it is
+	#determines whose move it is
 	def move
-		if turn == :playerone
+		if @turn % 2 == 1
 			player_one_turn
 		else
 			player_two_turn
@@ -26,65 +26,90 @@ class TicTacToe
 		@turn += 1
 	end
 
-	#determines whose turn it is
-	def turn
-		@turn = 1
-		if @turn % 2 == 1
-			return :playerone
-		else
-			return :playertwo
-		end	
+	def valid_move(row, col)
+		if @game_board.board[row][col] != " "
+			puts "There's already a shape there, try again."
+			move
+		end
 	end
-
 	#player ones turn
 	def player_one_turn
+		print_board
 		puts "#{@player1.name} it's your turn"
 		puts "Enter a row (0-2)"
 		row = gets.chomp.to_i
 		puts "Enter a column (0-2)"
 		col = gets.chomp.to_i
+		valid_move(row, col)
 		@game_board.board[row][col] = @player1.shape
 		print_board
-		winner if win?(@player1.shape)
-
+		if win?(@player1.shape)
+			winner(@player1.name)
+			@winner = true
+		end
 	end
 
 	#player two's turn
 	def player_two_turn
+		print_board
 		puts "#{@player2.name} it's your turn"
 		puts "Enter a row (0-2)"
 		row = gets.chomp.to_i
 		puts "Enter a column (0-2)"
 		col = gets.chomp.to_i
+		valid_move(row, col)
 		@game_board.board[row][col] = @player2.shape
 		print_board
-		winner(@player2.name) if win?(@player2.shape)
-	end
-
-	def win?(shape)
-		if (@game_board.board[0][0] == shape) && (@game_board.board[0][1] == shape) && (@game_board.board[0][2] == shape)
-			return true
-		elsif (@game_board.board[1][0] == shape) && (@game_board.board[1][1] == shape) && (@game_board.board[1][2] == shape)
-			return true
-		elsif (@game_board.board[2][0] == shape) && (@game_board.board[2][1] == shape) && (@game_board.board[2][2] == shape)
-			return true
-		elsif (@game_board.board[0][0] == shape) && (@game_board.board[1][0] == shape) && (@game_board.board[2][0] == shape)
-			return true
-		elsif (@game_board.board[0][1] == shape) && (@game_board.board[1][1] == shape) && (@game_board.board[2][1] == shape)
-			return true
-		elsif (@game_board.board[0][2] == shape) && (@game_board.board[1][2] == shape) && (@game_board.board[2][2] == shape)
-			return true
-		elsif (@game_board.board[0][0] == shape) && (@game_board.board[1][1] == shape) && (@game_board.board[2][2] == shape)
-			return true
-		elsif (@game_board.board[0][2] == shape) && (@game_board.board[1][1] == shape) && (@game_board.board[2][0] == shape)
-			return true
-		else
-			return false
+		if win?(@player2.shape)
+			winner(@player2.name)
+			@winner = true
 		end
 	end
 
+	def win?(shape)
+		unless draw?
+			if (@game_board.board[0][0] == shape) && (@game_board.board[0][1] == shape) && (@game_board.board[0][2] == shape)
+				return true
+			elsif (@game_board.board[1][0] == shape) && (@game_board.board[1][1] == shape) && (@game_board.board[1][2] == shape)
+				return true
+			elsif (@game_board.board[2][0] == shape) && (@game_board.board[2][1] == shape) && (@game_board.board[2][2] == shape)
+				return true
+			elsif (@game_board.board[0][0] == shape) && (@game_board.board[1][0] == shape) && (@game_board.board[2][0] == shape)
+				return true
+			elsif (@game_board.board[0][1] == shape) && (@game_board.board[1][1] == shape) && (@game_board.board[2][1] == shape)
+				return true
+			elsif (@game_board.board[0][2] == shape) && (@game_board.board[1][2] == shape) && (@game_board.board[2][2] == shape)
+				return true
+			elsif (@game_board.board[0][0] == shape) && (@game_board.board[1][1] == shape) && (@game_board.board[2][2] == shape)
+				return true
+			elsif (@game_board.board[0][2] == shape) && (@game_board.board[1][1] == shape) && (@game_board.board[2][0] == shape)
+				return true
+			else
+				return false
+			end
+		end
+	end
+
+	def draw?
+		if @turn > 8
+			puts "The game ended in a draw. :)"
+			@winner = true
+			return true
+		end
+		false
+	end
+
 	def winner(winner_name)
-		puts "You win!"
+		puts "#{winner_name}, YOU WIN!!!"
+	end
+
+	def play
+		@turn = 1
+		@winner = false
+
+		until @winner
+			move
+		end
 	end
 
 
@@ -107,7 +132,4 @@ class TicTacToe
 end
 
 game = TicTacToe.new
-game.player_one_turn
-game.player_one_turn
-game.player_one_turn
-
+game.play
