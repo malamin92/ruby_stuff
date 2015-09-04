963a59143007a9d3308d3a8647fa82341354b507
Mo Amin <m.alamin92@gmail.com>
2015-09-04 02:24:29 -0400

    added word picker and word setter methods

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,15 +1,36 @@
 class HangMan
+	attr_accessor :word, :bracket, :chances_left
 	def initialize
+		@bracket = ""
+		@word = ""
+		@chances_left = 7
 	end
 
 	def word_picker
 		words = File.readlines("5desk.txt")
 		@right_size_words = words.select{|word| (word.strip.length >= 5) && (word.length <= 12) }
-		@right_size_words
+		@right_size_words.sample
 	end
 
-	def random_word
+	def set_word
+		@word = word_picker
+	end
+
+	def set_bracket
+		i = 0
+		while i < @word.length
+			@bracket[i] = "_" 
+			i += 1
+		end
+		puts "#{@word.length} letters."
+	end
+
+
+
+
 end
 
 game = HangMan.new
-game.word_picker
+game.set_word
+game.set_bracket
+puts game.bracket
