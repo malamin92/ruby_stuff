95db238a504e3b83b55c54da15593f40dfc926c3
Mo Amin <m.alamin92@gmail.com>
2015-09-07 07:48:34 -0400

    finished twitter bot

--- twitter_bot/micro_blogger.rb
+++ twitter_bot/micro_blogger.rb
@@ -1,4 +1,5 @@
 require 'jumpstart_auth'
+require 'bitly'
 
 class MicroBlogger
 	attr_accessor :client
@@ -42,6 +43,26 @@ class MicroBlogger
   		end
   	end
 
+  	def everyones_last_tweet
+	    friends = @client.friends.collect { |friend| @client.user(friend)}
+	    friends = friends.sort_by {|friend| friend.screen_name }
+	    friends.each do |friend|
+		    last_tweet = friend.status.text
+		    timestamp = friend.status.created_at
+		    timestamp.strftime("%A, %b %d")
+		    puts "\n"
+		    puts friend.screen_name + " on #{timestamp}"
+		    puts last_tweet
+	    end
+  	end
+
+  	def shorten(original_url)
+    puts "Shortening this URL: #{original_url}"
+    Bitly.use_api_version_3
+    bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    return bitly.shorten(original_url).short_url
+  end
+
   	def run
   		puts "Welcome to the JSL Twitter Client."
   		command = ""
@@ -59,6 +80,12 @@ class MicroBlogger
 	  				dm(parts[1], parts[2..-1].join(" "))
 	  			when "spam" then
 	  				spam_my_followers(parts[1..-1].join(" "))
+	  			when "elt" then
+	  				everyones_last_tweet
+	  			when 's' then 
+	  				puts shorten(parts[1..-1].join(" "))
+	  			when 'turl' then 
+	  				tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
 	  			else
 	  				puts "Sorry, I don't know how to '#{command}'."
   			end
