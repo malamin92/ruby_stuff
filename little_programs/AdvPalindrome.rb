3060e0bae252a9013491cddbc871a437628c6249
Mo Amin <m.alamin92@gmail.com>
2015-10-09 08:47:32 -0400

    added a simpler version of the solution

--- little_programs/AdvPalindrome.rb
+++ little_programs/AdvPalindrome.rb
@@ -9,3 +9,9 @@ end
 
 puts true if adv_palindrome?("Anne, I vote more cars race Rome-to-Vienna")
 
+def adv_palindrome_simple?(str)
+	return true if str.gsub(/[\s+\W]/, "").downcase.reverse ==  str.gsub(/[\s+,\W]/, "").downcase
+	return false
+end
+
+puts adv_palindrome_simple?("Anne, I vote more cars race Rome-to-Vienna")
